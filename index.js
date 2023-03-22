e83d4dc204e8f4ab22adc4d90907eeed7d98229b
sawrubgupta <sgupta10008@gmail.com>
2023-03-22 09:18:00 +0530

    adding body parser

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 101;
+
+// app.use(bodyParser.json());
+
+// //support parsing of application/x-www-form-urlencoded post data
+// app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
-  res.send("Hello flutter guys !");
+  res.send("Hello flutter and marketing  guys !");
 });
+
 
 app.listen(port, () => {
   console.log(`server is runing on port ${port}!`);
